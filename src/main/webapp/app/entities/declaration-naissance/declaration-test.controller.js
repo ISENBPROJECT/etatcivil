ee854eaa7ce9d46900fb829deeec6760ac7e125c
cheikhahmadoutidianefall <cheikhahmadoutidianefall@gmail.com>
2016-11-05 12:16:06 +0100

    resolution conflit

--- src/main/webapp/app/entities/declaration-naissance/declaration-test.controller.js
+++ src/main/webapp/app/entities/declaration-naissance/declaration-test.controller.js
@@ -5,20 +5,20 @@
         .module('etatcivilApp')
         .controller('DeclarationTestController', DeclarationTestController);
 
-    DeclarationTestController.$inject = ['$timeout', '$scope', '$stateParams', 'entity', 'DeclarationNaissance', 'Personne','Fichier','Upload'];
+    DeclarationTestController.$inject = ['$timeout', '$scope', '$stateParams', 'entity', 'DeclarationNaissance', 'Personne', 'Fichier', 'Upload'];
 
-    function DeclarationTestController($timeout, $scope, $stateParams, entity, DeclarationNaissance, Personne,Fichier,Upload) {
+    function DeclarationTestController($timeout, $scope, $stateParams, entity, DeclarationNaissance, Personne, Fichier, Upload) {
 
         var vm = this;
         vm.declarationNaissance = entity;
         vm.datePickerOpenStatus = {};
         vm.openCalendar = openCalendar;
         vm.save = save;
-        vm.kaze = null;
         vm.datePickerOpenStatus.dateDeclaration = false;
         vm.datePickerOpenStatus.dateNaissance = false;
         vm.personnes = Personne.query();
         vm.fichiers = Fichier.query();
+        vm.uploadFiles = uploadFiles;
 
         function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
@@ -29,34 +29,16 @@
             DeclarationNaissance.save(vm.declarationNaissance);
         }
 
-        $scope.uploadFiles = function(files, errFiles) {
+        function uploadFiles(files, errFiles) {
             $scope.files = files;
             var filename1;
-            var filename2;
-            files.forEach(function(file) {
-                filename1  = file.name;
+            files.forEach(function (file) {
+                filename1 = file.name;
             })
-            DeclarationNaissance.upload(filename1);
-            $scope.errFiles = errFiles;
+            var filename2;
+            var fichier = {nomFichier: filename1, chemin: 'C:\/Users\/mroum\/OneDrive\/Documents\/repetatcivil'};
 
-          /*  angular.forEach(files, function(file) {
-                file.upload = Upload.upload({
-                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                    data: {file: file}
-                });
-
-                file.upload.then(function (response) {
-                    $timeout(function () {
-                        file.result = response.data;
-                    });
-                }, function (response) {
-                    if (response.status > 0)
-                        $scope.errorMsg = response.status + ': ' + response.data;
-                }, function (evt) {
-                    file.progress = Math.min(100, parseInt(100.0 *
-                        evt.loaded / evt.total));
-                });
-            });*/
+            vm.declarationNaissance.fichier = fichier;
         }
     }
 })();
